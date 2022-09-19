5478e0a86c4f97957727d5a096b07a4f76610e82
raghav <raghavjindal0212@gmail.com>
2022-09-19 20:57:53 +0530

    location

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 import { Landing } from "./Components/Landing/Landing";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { PreNav } from "./Components/PreNav/PreNav";
@@ -10,12 +12,34 @@ import Scrolltotop from "./Scrolltotop";
 import Location from "./Components/location/Location";
 import { Footer } from "./Components/Footer/Footer";
 function App() {
+  const [countryName, setcountryName] = useState("");
+  const [countryCode, setcountryCode] = useState("");
+  const [city, setcity] = useState("");
+  const [region, setregion] = useState("");
+  const getGeoInfo = () => {
+    axios
+      .get("https://ipapi.co/json/")
+      .then((response) => {
+        let data = response.data;
+
+        setcountryName(data.country_name);
+        setcountryCode(data.country_calling_code);
+        setcity(data.city);
+        setregion(data.region);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    getGeoInfo();
+  }, []);
   return (
     <div className="App">
       <PreNav></PreNav>
       <BrowserRouter>
         <Scrolltotop></Scrolltotop>
-        <Navbar />
+        <Navbar countryName={countryName} city={city} region={region} />
         <Routes>
           <Route path="/" element={<Landing />}></Route>
           <Route path="/wedding" element={<Wedding />}></Route>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,61 +1,67 @@
+import { Box } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Navbar.css";
 
-export const Navbar = () => {
-	const navigate = useNavigate();
+export const Navbar = ({ countryName, city, region }) => {
+  const navigate = useNavigate();
 
-	return (
-		<div className="navcontainer">
-			<div className="navinnercontainer">
-				<div
-					className="navlogocontainer"
-					onClick={() => {
-						navigate("/");
-					}}
-				>
-					<span>Tina Rosario</span>
-				</div>
-				<div className="navoptions">
-					<span className="navlink1">Gallery</span>
-					<span
-						className="navlink2"
-						onClick={() => {
-							// document.getElementById("contactusdiv").scrollIntoView();
-							document.getElementById("testimonialsdiv").scrollIntoView({
-								behavior: "smooth"
-							});
-						}}
-					>
-						Testimonials
-					</span>
-					<span className="navlink3">About Us</span>
-					<span
-						className="navlink5"
-						onClick={() => {
-							navigate("/fabrics");
-						}}
-					>
-						Fabrics
-					</span>
-					<span
-						onClick={() => {
-							// document.getElementById("contactusdiv").scrollIntoView();
-							document.getElementById("contactusdiv").scrollIntoView({
-								behavior: "smooth"
-							});
-						}}
-						className="navlink4"
-					>
-						Contact Us
-					</span>
-				</div>
-				<div className="navicons">
-					<i class="fa-solid fa-magnifying-glass"></i>
-					<i class="fa-solid fa-cart-shopping"></i>
-					<i class="fa-solid fa-user"></i>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="navcontainer">
+      <div className="navinnercontainer">
+        <div
+          className="navlogocontainer"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <span>Tina Rosario</span>
+        </div>
+        <div className="navoptions">
+          <span className="navlink1">Gallery</span>
+          <span
+            className="navlink2"
+            onClick={() => {
+              // document.getElementById("contactusdiv").scrollIntoView();
+              document.getElementById("testimonialsdiv").scrollIntoView({
+                behavior: "smooth",
+              });
+            }}
+          >
+            Testimonials
+          </span>
+          <span className="navlink3">About Us</span>
+          <span
+            className="navlink5"
+            onClick={() => {
+              navigate("/fabrics");
+            }}
+          >
+            Fabrics
+          </span>
+          <span
+            onClick={() => {
+              // document.getElementById("contactusdiv").scrollIntoView();
+              document.getElementById("contactusdiv").scrollIntoView({
+                behavior: "smooth",
+              });
+            }}
+            className="navlink4"
+          >
+            Contact Us
+          </span>
+        </div>
+        <div className="navicons">
+          <i class="fa-solid fa-magnifying-glass"></i>
+          <i class="fa-solid fa-cart-shopping"></i>
+          <i class="fa-solid fa-user"></i>
+        </div>
+        <div className="location">
+          <p>{city},</p>
+          <p>{region},</p>
+          <p>{countryName}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
